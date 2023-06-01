f7d5f7d3553e4d4a8bfbfa67d2a4fe115c3841b0
AKO PRECIOUS <akoprecious58@gmail.com>
2023-06-01 10:36:24 -0400

    Encrypt/Decrypt

--- AssignmentDataSecurityEncoding/Program.cs
+++ AssignmentDataSecurityEncoding/Program.cs
@@ -1,13 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using AssignmentDataSecurityEncoding;
-
-
-Console.WriteLine("/n ===== Task 1 - User Input ======= /n ");
+Console.WriteLine("/n ===== Task 1 - User Name ======= /n ");
 Console.WriteLine("What is Your name");
-string inputStr = Console.ReadLine();
+string nameStr = Console.ReadLine();
 
-
+Console.WriteLine("/n ===== Task 2 - Binary Convertion ======= /n ");
 BinaryConverter  binary= new BinaryConverter();
-string binaryData = binary.StringToBinary(inputStr);
+string binaryData = binary.StringToBinary(nameStr);
+Console.WriteLine(binaryData);
 binary.BinaryToString(binaryData);
+
+Console.WriteLine("/n ===== Task 3 - hexadecimal  Convertion ======= /n ");
+HexadecimalConverter hexaDec = new HexadecimalConverter();
+string hexData = hexaDec.StringToHexadecimal(nameStr);
+Console.WriteLine(hexData);
+hexaDec.HexadecimalToString(hexData);
+
+Console.WriteLine("/n ===== Task 4 - Base64  Convertion ======= /n ");
+Base64Converter base64 = new Base64Converter();
+string base64Data = base64.StringToBase64(nameStr);
+Console.WriteLine(base64Data);
+base64.Base64ToString(base64Data);
+
+Console.WriteLine("/n ===== Task 5 - Encrypt/Decrypt ======= /n ");
+EncryptDecrypt encrypt = new EncryptDecrypt();
+var encryptData = encrypt.EncryptString(nameStr);
+Console.WriteLine(encryptData);
+encrypt.DecryptString(encryptData.Item1, encryptData.Item2, encryptData.Item3);
