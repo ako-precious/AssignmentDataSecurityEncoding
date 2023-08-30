06cfc50e73fca8df1bd72ba04e32f6cb50cb5988
AKO PRECIOUS <akoprecious58@gmail.com>
2023-08-29 22:43:39 -0400

    init commit

---
+++ Laravel/larazillow/resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    
+        <div>Index</div>
+        <Link href="/show">Show Pages</Link>
+        <div>The message is {{ message }}</div>
+   
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps(["mesage"]);
+</script>
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+export default {
+   layout: MainLayout
+}
+</script>
